152ca239d31e1314b7297fd0152dc132fbd37468
marlonbbritto <marlonbbritto@gmail.com>
2025-05-10 22:13:40 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -92,6 +92,14 @@ public class Order implements Serializable {
         return items;
     }
 
+    public Double getTotal(){
+        double sum = 0;
+        for(OrderItem item: items){
+            sum += item.getSubTotal();
+        }
+        return sum;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
